0792d8aa0f11a97d6caee8d3dcdb4d3414e57600
username <athulraj165@gmail.com>
2023-11-10 19:47:27 +0530

    Type script Infer types

---
+++ src/Components/Javascript basic/Javascript.tsx
@@ -0,0 +1,6 @@
+// ------------------- Infertypes
+// inferTypes with means type will define automatically when initial 
+// declaretion was happened
+// TypeScript will automatically verify the types
+let userName = 'athul raj'
+// userName=0 if we use this it will shows an error
