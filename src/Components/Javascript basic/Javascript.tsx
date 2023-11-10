f700b6025b94efc950eb28b6f0d8be6d653aa963
username <athulraj165@gmail.com>
2023-11-10 20:03:22 +0530

    explicit types in TypeScript

--- src/Components/Javascript basic/Javascript.tsx
+++ src/Components/Javascript basic/Javascript.tsx
@@ -4,3 +4,23 @@
 // TypeScript will automatically verify the types
 let userName = 'athul raj'
 // userName=0 if we use this it will shows an error
+
+
+// ---------------Defining types or (Explicit Types)
+let explicitName : string = 'athulraj';
+let explicitNumber : number = 1000;
+let explicitBoolean : boolean = true
+let explicitMySkills :string[]= ['React,Js,CSS']
+let explicitNumbers :number[]= [1,2,3,4,5];
+let explicitMyDetails :{
+    name:string;
+    age:number;
+    salary:number;
+    is_working:boolean;
+}={
+    name:'Athulraj',
+    age:24,
+    salary:4,
+    is_working:false
+}
+// -------------------This is the overAll explicit Types 
