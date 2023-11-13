0e1ef03d985c0637d7ce93095e2d95c6fbc9bbe1
username <athulraj165@gmail.com>
2023-11-13 14:40:43 +0530

    prototype example

--- src/Components/Javascript basic/javascriptTest.js
+++ src/Components/Javascript basic/javascriptTest.js
@@ -22,20 +22,41 @@
 //   console.log(combinedInfo);
   
 // Original object
-const originalObject = {
-    name: 'John',
-    age: 30,
-    address: {
-      city: 'New York',
-      country: 'USA'
-    }
+// const originalObject = {
+//     name: 'John',
+//     age: 30,
+//     address: {
+//       city: 'New York',
+//       country: 'USA'
+//     }
+//   };
+//   // Deep copy using JSON.parse() and JSON.stringify()
+//   const deepCopy = JSON.parse(JSON.stringify(originalObject));
+//   // Modify the deep copy
+//   deepCopy.age = 31;
+//   deepCopy.address.city = 'San Francisco';
+//   // Changes in the deep copy do not affect the original object
+//   console.log(originalObject);
+//   console.log(deepCopy);
+  
+  
+
+// Constructor function for a basic person object
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  
+  // Adding a method to the prototype of the Person constructor
+  Person.prototype.prototypeExample = function () {
+    console.log(`Hello, my name is ${this.name} and I'm ${this.age} years old.`);
   };
-  // Deep copy using JSON.parse() and JSON.stringify()
-  const deepCopy = JSON.parse(JSON.stringify(originalObject));
-  // Modify the deep copy
-  deepCopy.age = 31;
-  deepCopy.address.city = 'San Francisco';
-  // Changes in the deep copy do not affect the original object
-  console.log(originalObject);
-  console.log(deepCopy);
+  
+  // Creating instances of the Person object
+  const person1 = new Person('Alice', 25);
+  const person2 = new Person('Bob', 30);
+  
+  // Calling the method from the prototype
+  person1.prototypeExample();
+  person2.prototypeExample();
   
