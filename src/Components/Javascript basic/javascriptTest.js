81dbcb69776dee82a3af48fecc43bab69e9f91f3
username <athulraj165@gmail.com>
2023-11-13 14:24:39 +0530

    shallow copy example

--- src/Components/Javascript basic/javascriptTest.js
+++ src/Components/Javascript basic/javascriptTest.js
@@ -1,41 +1,44 @@
-const objectOrginal = {
-  name: "John",
-  age: 30,
-  address: {
-    city: "New York",
-    country: "USA",
-  },
-};
-
-const shallowCopy = Object.assign({}, originalObject);
-
-// Modify the shallow copy
-shallowCopy.age = 31;
-shallowCopy.address.city = 'San Francisco';
-
-// Changes are reflected in the original object's address as well
-console.log(originalObject);
-console.log(shallowCopy);
-
-
-// Original object
-const originalObject = {
-    name: 'John',
-    age: 30,
-    address: {
-      city: 'New York',
-      country: 'USA'
-    }
+// Source objects
+const personDetails = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 30
   };
   
-  // Deep copy using JSON.parse() and JSON.stringify()
-  const deepCopy = JSON.parse(JSON.stringify(originalObject));
+  const addressInfo = {
+    city: 'New York',
+    country: 'USA'
+  };
   
-  // Modify the deep copy
-  deepCopy.age = 31;
-  deepCopy.address.city = 'San Francisco';
+  const combinedInfo =  Object.assign({},addressInfo,personDetails)
   
-  // Changes in the deep copy do not affect the original object
-  console.log(originalObject);
-  console.log(deepCopy);
+  // Modify the combined object
+  combinedInfo.age = 31;
+  combinedInfo.city = 'San Francisco';
+  
+  // Original objects and the combined object
+  console.log(personDetails);
+  console.log(addressInfo);
+  console.log(combinedInfo);
+  
+// // Original object
+// const originalObject = {
+//     name: 'John',
+//     age: 30,
+//     address: {
+//       city: 'New York',
+//       country: 'USA'
+//     }
+//   };
+  
+//   // Deep copy using JSON.parse() and JSON.stringify()
+//   const deepCopy = JSON.parse(JSON.stringify(originalObject));
+  
+//   // Modify the deep copy
+//   deepCopy.age = 31;
+//   deepCopy.address.city = 'San Francisco';
+  
+//   // Changes in the deep copy do not affect the original object
+//   console.log(originalObject);
+//   console.log(deepCopy);
   
