f72b920b21aaf30213b66f1be82ae8d9eef854fa
username <athulraj165@gmail.com>
2023-11-13 14:15:18 +0530

    deepCopy

--- src/Components/Javascript basic/javascriptTest.js
+++ src/Components/Javascript basic/javascriptTest.js
@@ -17,3 +17,25 @@ shallowCopy.address.city = 'San Francisco';
 console.log(originalObject);
 console.log(shallowCopy);
 
+
+// Original object
+const originalObject = {
+    name: 'John',
+    age: 30,
+    address: {
+      city: 'New York',
+      country: 'USA'
+    }
+  };
+  
+  // Deep copy using JSON.parse() and JSON.stringify()
+  const deepCopy = JSON.parse(JSON.stringify(originalObject));
+  
+  // Modify the deep copy
+  deepCopy.age = 31;
+  deepCopy.address.city = 'San Francisco';
+  
+  // Changes in the deep copy do not affect the original object
+  console.log(originalObject);
+  console.log(deepCopy);
+  
