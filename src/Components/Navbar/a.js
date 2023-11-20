1f43b4b4dac6c64cf2bbf974937ebb92f852b7d6
username <athulraj165@gmail.com>
2023-11-20 15:58:48 +0530

    data structure

--- src/Components/Navbar/a.js
+++ src/Components/Navbar/a.js
@@ -1,10 +1,10 @@
-let moveZeroes = function(nums) {
+var moveZeroes = function(nums) {
     if(nums.length<1){
         return false
     }
-    let result =  nums.sort((a, b)=> b-a)
-    return result
-};
+    return nums.sort((a, b)=> (a)-(b))
+
+    };
 
 let nums = [0,1,0,3,12,0,1,0];
 let k = moveZeroes(nums)
