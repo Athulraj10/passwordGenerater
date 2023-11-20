d01b600ad4e8d15194f8809d389860a61d51a26d
username <athulraj165@gmail.com>
2023-11-20 16:01:49 +0530

    0 and 1 move to first position

--- src/Components/Navbar/a.js
+++ src/Components/Navbar/a.js
@@ -2,8 +2,7 @@ var moveZeroes = function(nums) {
     if(nums.length<1){
         return false
     }
-    return nums.sort((a, b)=> (a)-(b))
-
+    return nums.sort((a, b)=> (b==1||b==0)-(a==1||a==0)).sort()
     };
 
 let nums = [0,1,0,3,12,0,1,0];
