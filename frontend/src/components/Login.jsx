441960172340ce6786284689706c50de2c6841e9
username <athulraj165@gmail.com>
2023-11-20 17:06:56 +0530

    login

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { setCredentials } from '../slices/authSlice'
 import { useLoginMutation } from '../slices/usersApiSlice';
 import { toast } from 'react-toastify'
+import backgroundImage from './navbar.jpg'
 
 const Login = () => {
   const [email, setEmail] = useState('');
@@ -40,29 +41,28 @@ const Login = () => {
     }
   },[userInfo,navigate])
   const divStyle = {
-    height: '80%',
+    height: '100%',
     display: 'flex',
     justifyContent: 'center',
-    background: 'url("background.jpeg") center/cover',  // Add your image path here
   };
   return (
     <div style={divStyle}>
-        <div style={{background:'black'}} className='md:w-2/6 h-fit rounded-lg mt-20'>
-            <div style={{background:"black"}} className='text-center p-2 rounded-tr-lg rounded-tl-lg'>
+        <div style={{ background: 'rgba(0, 0, 0, 0.5)' }} className='md:w-2/6 h-fit rounded-lg mt-20'>
+            <div style={{ background: 'rgba(0, 0, 0, 0.3)'}} className='text-center p-5 rounded-tr-lg rounded-tl-lg'>
                 <h1 className='text-white font-bold text-2xl'>Login</h1>
             </div>
             <form className='p-5' onSubmit={submitHandler}>
-                <div className='my-2'>
-                    <label className='text-primaryColor font-bold' htmlFor="email">Email</label>
-                    <input type="email" value={email} onChange={(e)=>{setEmail(e.target.value)}} id='email' className='w-full focus:outline-none focus:border-b-2 border-primaryColor p-1'/>
+                <div className='my-6'>
+                    <label style={{color:'silver',fontSize:"20px"}} className=' font-bold' htmlFor="email">Email</label>
+                    <input type="email" value={email} onChange={(e)=>{setEmail(e.target.value)}} id='email' placeholder='Enter Email' className='w-full focus:outline-none focus:border-b-4 border-primaryColor p-3'/>
                 </div>
-                <div className='my-2'>
-                    <label className='text-primaryColor font-bold' htmlFor="password">Password</label>
-                    <input type="password" value={password} onChange={(e)=>{setPassword(e.target.value)}} id='password' className='w-full focus:outline-none focus:border-b-2 border-primaryColor p-1'/>
+                <div className='my-6'>
+                    <label style={{color:'silver',fontSize:"20px"}} className=' font-bold' htmlFor="password">Password</label>
+                    <input type="password" value={password} onChange={(e)=>{setPassword(e.target.value)}} id='password' placeholder='Enter Password' className='w-full focus:outline-none focus:border-b-4 border-primaryColor p-3'/>
                 </div>
                 <div className="mt-3 flex justify-between">
-                <button type="submit" className='bg-primaryColor hover:bg-secondaryColor px-3 py-1 rounded-lg text-secondaryColor hover:text-primaryColor hover:border-2 border-primaryColor font-medium'>Login</button>
-                  <Link to='/register'><h6 className='text-primaryColor hover:underline'>Register</h6></Link>
+                <button type="submit" style={{background:"gray",color:'black',fontSize:"20px"}} className='hover:bg-secondaryColor px-3 py-1 rounded-lg text-secondaryColor hover:text-primaryColor hover:border-2 border-primaryColor font-medium'>Login</button>
+                  <Link to='/register'><h6 style={{color:'silver',fontSize:"20px"}} className=' hover:underline'>Register</h6></Link>
                 </div>
             </form>
         </div>
