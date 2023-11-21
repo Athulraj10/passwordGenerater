81f1b71bb603d925939aaedac6b3eff84f046485
username <athulraj165@gmail.com>
2023-11-21 13:54:27 +0530

    src added

---
+++ srccccc/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+
+class App extends Component {
+  render() {
+    return (
+     <Navbar/>
+    );
+  }
+}
+
+export default App;
