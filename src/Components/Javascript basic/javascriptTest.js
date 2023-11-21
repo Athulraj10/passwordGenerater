95d9dd5df038a8e2d6bd1e12e3e9ab7b2e9c0d5b
username <athulraj165@gmail.com>
2023-11-21 13:54:43 +0530

    types defined

--- src/Components/Javascript basic/javascriptTest.js
+++
@@ -1,8 +0,0 @@
-var thirdMax = function(nums) {
-  let arrayValues = Array.from(new Set(nums.sort((a,b) => b-a)))
-  if(arrayValues.length < 3) return arrayValues[0]
-  return arrayValues[2]
-};
-
-let nums = [3,2,1];
-console.log(thirdMax(nums))
